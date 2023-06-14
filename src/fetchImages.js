d748a67d35793236391125e98dac71d4e9e1084e
Andriy2016 <122678717+Andriy2016@users.noreply.github.com>
2023-06-14 15:42:21 +0300

    Update fetchImages.js

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -5,8 +5,5 @@ export async function fetchImages(name, page) {
   const resp = await axios.get(
     `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
   );
-  if (!resp.ok) {
-    throw new Error(resp.statusText);
-  }
   return await resp.data;
 }
